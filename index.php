7d9ff0f8c1b5970890fffc62068e0ea1aa3673fd
Peter-S <32533746+Peter-S@users.noreply.github.com>
2019-01-27 19:10:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
 	$username = "root";
-	$password = "s1n2o3e4";
+	$password = "";
 	$dbname = "shopping";
 	
 	$conn = new mysqli($servername, $username, $password, $dbname);
